6fd65fdcd33a9b0015609d3454181d92a8888b1c
dcrmk67 <61564443+dcrmk67@users.noreply.github.com>
2021-03-28 15:12:27 -0500

    Initial commit.

--- script_create_data.R
+++ script_create_data.R
@@ -5,27 +5,42 @@
 # Created by: Davin Cermak
 # Created on: 28MAR2021
 
-library(tidyverse)
-library(lubridate)
+#############################################################
+# Load required packages
+# NOTE: This code will install the tidyverse package to the 
+# user's machine if it is not already available.
+#############################################################
 
+if (!require('tidyverse')) install.packages('tidyverse')
+
+#############################################################
+# Download zip files to local machine
+#############################################################
 download.file('https://data.bls.gov/cew/data/files/2020/csv/2020_qtrly_singlefile.zip','./data/qcew2020.zip')
 download.file('https://data.bls.gov/cew/data/files/2019/csv/2019_qtrly_singlefile.zip','./data/qcew2019.zip')
 
+
+#############################################################
+# Unzip and create data from of QCEW data
+#############################################################
+
+# Assigns value to access and save files in the project's 
+# data directory
+
 dataDir <- './data'
 
+# Unzip downloaded files 
 zipfiles <- list.files(path=dataDir, pattern='.zip', full.names=TRUE) 
-
 for (file in zipfiles) {
    unzip(file, exdir=dataDir)
 }
 
+# Map csv files into a single data frame
 csvfiles <- list.files(path=dataDir, pattern='.csv', full.names=TRUE)
-
 data <- map_df(csvfiles, read_csv, col_names=TRUE)
 
-rm(dataDir,file,zipfiles,csvfiles)
-
-
+# Create a separate data file for county-level QCEW data and some initial
+# data cleanup
 cntydata <- data %>%
    filter(size_code==0 
           & own_code==5 
@@ -34,10 +49,12 @@ cntydata <- data %>%
           & substr(area_fips,1,2) <= 56) %>%
    mutate(empl = round(((month1_emplvl+month2_emplvl+month3_emplvl) / 3), digits=0),
           empl = na_if(empl,0),
-          date = yq(paste0(as.character(year),'-',qtr))) %>%
+          date = paste0(as.character(year),'-',qtr)) %>%
    rename(estabs = qtrly_estabs) %>%
    select(area_fips, industry_code, agglvl_code, date, estabs, empl)
 
-rm(data)
+# Global environment cleanup
+rm(dataDir,file,zipfiles,csvfiles,data)
 
+# Save county-level data for additional analyses
 saveRDS(cntydata, file='./data/cntydata.rda')
